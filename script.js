f114caf47ce921773c71ff6486d2d6341f0a9680
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-09 20:14:12 +0330

    Handle hovering over squares

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let dimension = 10; //Must take it from input later
+let dimension = 50; //Must take it from input later
 const cell = document.createElement("div");
 cell.classList.add("square-cell");
 cell.style.width = `calc(100%*(1/${dimension}) - 2px)`;
 
 const gridContainer = document.querySelector(".grid-container");
-for (let i = 0; i < dimension*dimension; i++) {
+for (let i = 0; i < dimension * dimension; i++) {
     gridContainer.appendChild(cell.cloneNode(false));
 }
+
+gridContainer.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "black";
+})
