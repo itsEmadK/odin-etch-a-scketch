4536b1fbf41391e8b33422b2fd1b277f6ec1e279
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-09 19:43:11 +0330

    Append square cells to the grid container

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cell = document.createElement("div");
 cell.classList.add("square-cell");
 cell.style.width = `calc(100%*(1/${dimension}) - 2px)`;
 
+const gridContainer = document.querySelector(".grid-container");
+for (let i = 0; i < dimension*dimension; i++) {
+    gridContainer.appendChild(cell.cloneNode(false));
+}
